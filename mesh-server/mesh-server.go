fc1bdc3265e005902a9c2891edc4bf17939c7e57
Joel Butler <joel@jhbutler.info>
2020-08-16 10:50:41 -0400

    first pass at go webserver

---
+++ mesh-server/mesh-server.go
@@ -0,0 +1,18 @@
+package main
+
+//web server derived from https://golang.org/doc/articles/wiki/
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
